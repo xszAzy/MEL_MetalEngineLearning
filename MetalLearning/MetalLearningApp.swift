678cef95c4c51d3af1032bd791b42de7a104fee8
xszAzy <3486170277@qq.com>
2025-09-25 18:10:23 +0800

    Initial Commit

---
+++ MetalLearning/MetalLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearningApp.swift
+//  MetalLearning
+//
+//  Created by 徐殿欢 on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
